02b57ff8f88ac5ed91ea6386021b3f707f304a62
chioukhlyes <lchioukh@excilys.com>
2016-02-09 23:32:11 +0100

    [Basics]Module concept

---
+++ actors.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Lyes CHIOUKH on 09/02/2016.
+ */
+
+/**
+ * @name printActor
+ */
+function printActor(){
+    console.log(" Actor 1");
+}
+
+/**
+ * @name printFilm
+ */
+function printFilm(){
+    console.log(" Film 1");
+}
+
+/**
+ * way to export module function/attribute
+ */
+module.exports.printActor = printActor;
---
+++ main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Lyes CHIOUKH on 09/02/2016.
+ */
+
+/**
+ * way to get external modules
+ * @type {exports|module.exports}
+ */
+var actors = require('./actors');
+
+//show console.log message
+actors.printActor();
+
+//we get an error here
+actors.printFilm();
